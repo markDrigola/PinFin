b9831c8525bbbc3becb746dac7be90a31bc914ef
Marik <mark_bass@mail.ua>
2016-12-06 23:14:15 +0200

    first commit index

---
+++ js/common.js
@@ -0,0 +1,26 @@
+'use strict'
+;(function () {
+    $(document).on('ready', function () {
+        $('.slider1').slick({
+            dots: true,
+            infinite: true,
+            speed: 300,
+            arrow: true,
+            slidesToShow: 1
+        });
+
+
+        //Filters accordion
+        $('.titleAccord').on('click', function () {
+            if($(this).parent().parent().find('div').hasClass('accordToggleSlide')) {
+                if($(this).parent().hasClass('notActiveAccords')) {
+                    $(this).parent().removeClass('notActiveAccords');
+                } else {
+                    $(this).parent().addClass('notActiveAccords');
+                }
+
+                $(this).parent().parent().find('.accordToggleSlide').slideToggle();
+            }
+        })
+    });
+})();
